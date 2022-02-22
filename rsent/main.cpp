f5e733c1d88d7093f2f77e4d4e80cca2f6d533e8
DennisUnplugged <80547849+DennisUnplugged@users.noreply.github.com>
2022-02-22 21:12:15 +0200

    Initial Commit

---
+++ rsent/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  rsent
+//
+//  Created by Dennis Gyftakis on 22/2/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
